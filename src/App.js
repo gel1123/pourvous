323c41d169753a9c16a83feb7aca7f9f80821c58
gel1123 <atno.14@gmail.com>
2021-08-17 08:51:27 +0900

    hoge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import header_img from './img/header_chocolat_2.jpg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const onClickHam = (e) => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <div>
-      <div className="bd-example">  
-        <ul className="dropdown-menu">
-          <li><h6 className="dropdown-header">Dropdown header</h6></li>
-          <li><a className="dropdown-item" href="#">Action</a></li>
-          <li><a className="dropdown-item" href="#">Another action</a></li>
-        </ul>
+    <div className="app-base">
+      <div className="app-header">
+        <span className="app-header-ham" onClick={onClickHam}>≡</span>
+        <span className="app-header-center">header</span>
       </div>
-      <div className="container">
-        <img src={header_img} style={{width:"100%"}}/>
+      <div className="app-header-dummy">
+      </div>
+      <div className="app-menu" style={
+        showMenu ? {
+          opacity: 1,
+          height: "100%"
+        } : {
+          opacity: 0,
+          height: "0%"
+        }
+      }>
+        menu
+      </div>
+      <div className="app-heading">
+        heading
+      </div>
+      <div className="app-main">
+        <div className="app-card app-card-wide">
+          card1
+        </div>
+        <div className="app-card">
+          card2
+        </div>
+        <div className="app-card">
+          card3
+        </div>
+        <div className="app-card">
+          card4
+        </div>
+        <div className="app-card">
+          card5
+        </div>
+        <div className="app-card">
+          card6
+        </div>
+        <div className="app-card">
+          card7
+        </div>
+        <div className="app-card">
+          card8
+        </div>
       </div>
     </div>
   );
