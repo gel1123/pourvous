aae2fa69d90f5b9c21441b81613212e1ddb62e00
gel1123 <atno.14@gmail.com>
2021-08-19 04:19:56 +0900

    hoge

--- src/AppCard.js
+++ src/AppCard.js
@@ -13,41 +13,52 @@ function AppCard(props) {
       const interval = setInterval(() => {
         console.log((index+1)%d.imgList.length, d.imgList[(index+1)%d.imgList.length]);
         setIndex((index+1)%d.imgList.length);
-      }, 4000);
+      }, 4500);
       return () => clearInterval(interval);
     }
   }, [index, d.imgList]);
 
   return (
-  <div className={"app-card " + (d.classappend ? d.classappend : "")}>
-    <div>
-      {d.name ? <h3 className="app-card-name">{d.name}</h3> : null}
-    </div>
-    <div className="imgbox">
-      {
-        d.imgList && d.imgList.map((image, i) => {
-          return index === i ? 
-            <img src={image} alt={d.name + "_画像"} className="app-card-img" key={i}/>
+  <div>
+    {d.title ? <h2 className="app-card-title">{d.title}</h2> : null}
+    <div className="app-card">
+      <div>
+        {
+          d.nameList ? <h3 className="app-card-name">＊{d.nameList[index]}</h3> : null
+        }
+      </div>
+      <div className="imgbox">
+        {
+          d.imgList && d.imgList.map((image, i) => {
+            return <img
+              src={image} alt={d.name + "_画像"} className="app-card-img" key={i}
+              style={
+                i === index ?
+                  {opacity: 1}
+                  : {opacity: 0}
+              }
+            
+            />
+          })
+        }
+      </div>
+      <div className="imgbox-dummy">
+        {
+          d.imgList  ? 
+            <img src={d.imgList[0]} alt={d.name + "_画像"} className="app-card-img-dummy" key={"dummy_"+d.imgList[0]}/>
             : null
-        })
-      }
+        }
+      </div>
+      <div>
+        {
+          d.imgList && d.imgList.map((image, i) => {
+            return index === i ? 
+              <div className="app-card-mes">{d.mesList[i]}</div>
+              : null
+          })
+        }
+      </div>    
     </div>
-    <div className="imgbox-dummy">
-      {
-        d.imgList  ? 
-          <img src={d.imgList[0]} alt={d.name + "_画像"} className="app-card-img" key={"dummy_"+d.imgList[0]}/>
-          : null
-      }
-    </div>
-    <div>
-      {
-        d.imgList && d.imgList.map((image, i) => {
-          return index === i ? 
-            <div className="app-card-mes">{d.mesList[i]}</div>
-            : null
-        })
-      }
-    </div>    
   </div>
   );
 } 
